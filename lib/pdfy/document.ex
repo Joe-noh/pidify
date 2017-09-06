b72a42da0a93851e169fd6639067b03278d1d00e
Joe-noh <goflb.jh@gmail.com>
2017-09-07 01:16:43 +0900

    tempfiles should have random name

--- lib/pdfy/document.ex
+++ lib/pdfy/document.ex
@@ -1,11 +1,23 @@
 defmodule Pdfy.Document do
-  alias Pdfy.Document.Pdf
 
   def generate_pdf(html) do
-    with :ok <- File.write("/tmp/a.html", html),
-         {_, 0} <- System.cmd("wkhtmltopdf", ["--quiet", "/tmp/a.html", "/tmp/a.pdf"]) do
+    html_path = tempfile_path(:html)
+    pdf_path  = tempfile_path(:pdf)
 
-      {:ok, "/tmp/a.pdf"}
+    options = ["--quiet", html_path, pdf_path]
+
+    with :ok <- File.write(html_path, html),
+         {_, 0} <- System.cmd("wkhtmltopdf", options) do
+      {:ok, pdf_path}
     end
+  end
+
+  defp tempfile_path(:html), do: "/tmp/#{random_string()}.html"
+  defp tempfile_path(:pdf),  do: "/tmp/#{random_string()}.pdf"
+
+  def random_string do
+    :crypto.strong_rand_bytes(24)
+    |> Base.url_encode64
+    |> binary_part(0, 24)
   end
 end
